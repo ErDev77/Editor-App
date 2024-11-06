8c81047bdb8f373e28bd891a244e6456ac20cc13
ErDev77 <122884246+ErDev77@users.noreply.github.com>
2024-11-07 03:05:11 +0400

    Update zustand-context.tsx

--- lib/zustand-context.tsx
+++ lib/zustand-context.tsx
@@ -1,23 +1,23 @@
 import React from "react";
 import { StoreApi } from "zustand";
 
-export const createZustandContext = <TInitial, TStore extends StoreApi<any>> (
+export const createZustandContext = <TInitial, TStore extends StoreApi<unknown>>(
     getStore: (initial: TInitial) => TStore
 ) => {
-    const Context = React.createContext(null as any as TStore)
+    const Context = React.createContext(null as unknown as TStore);
 
     const Provider = (props: {
-        children?: React.ReactNode
-        initialValue: TInitial
+        children?: React.ReactNode;
+        initialValue: TInitial;
     }) => {
-        const [store] = React.useState(getStore(props.initialValue))
+        const [store] = React.useState(getStore(props.initialValue));
 
-        return <Context.Provider value={store}>{props.children}</Context.Provider>
-    }
+        return <Context.Provider value={store}>{props.children}</Context.Provider>;
+    };
 
     return {
-        useContext: () => React.useContext(Context),
+        useContext: (): TStore => React.useContext(Context),
         Context,
-        Provider
-    }
-}
+        Provider,
+    };
+};
