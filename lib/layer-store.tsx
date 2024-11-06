b9c614e543480a4b41e60e0ee9676705640f3c12
ErDev77 <122884246+ErDev77@users.noreply.github.com>
2024-11-07 03:10:12 +0400

    Update layer-store.tsx

--- lib/layer-store.tsx
+++ lib/layer-store.tsx
@@ -3,7 +3,7 @@ import { StoreApi, useStore } from 'zustand'
 import React from 'react'
 import { persist } from 'zustand/middleware'
 
-const createZustandContext = <TInitial, TStore extends StoreApi<any>>(
+const createZustandContext = <TInitial, TStore extends StoreApi<State>>(
 	getStore: (initial: TInitial) => TStore
 ) => {
 	const Context = React.createContext(null as unknown as TStore)
